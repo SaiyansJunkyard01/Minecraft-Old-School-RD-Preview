6358a7222b0012d8343fc7fb6f74161c17c527d7
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2024-11-26 20:24:49 -0500

    Update BiomeGenRainforest.java

--- src/main/java/net/minecraft/src/BiomeGenRainforest.java
+++ src/main/java/net/minecraft/src/BiomeGenRainforest.java
@@ -4,4 +4,8 @@ public class BiomeGenRainforest extends BiomeGenBase {
 	public WorldGenerator getRandomWorldGenForTrees(Random var1) {
 		return (WorldGenerator)(var1.nextInt(3) == 0 ? new WorldGenBigTree() : new WorldGenTrees());
 	}
+	
+	public int getSkyColorByTemp(float var1) {
+		return 1120580;
+	}
 }
